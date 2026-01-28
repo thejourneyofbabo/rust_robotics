ba128a7d4cb8796b763c631f7c1feb11e6b37c34
JisangYun <journeyofbabo@gmail.com>
2026-01-28 09:37:41 +0900

    Doing: BFS

--- src/path_planning/breadth_first_search.rs
+++ src/path_planning/breadth_first_search.rs
@@ -1,7 +1,14 @@
 use crate::utils::math;
 
-pub struct GoalPosition {}
-pub struct ObstaclePosition {}
+pub struct StartAndGoalPosition {
+    pub start: (f32, f32),
+    pub end: (f32, f32),
+}
+
+pub struct ObstaclePosition<T> {
+    pub ox: Vec<T>,
+    pub oy: Vec<T>,
+}
 
 pub fn use_math() -> i32 {
     let a = math::sum(2, 3);
