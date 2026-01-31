16b219cc5195650e29784f5a6fd8ec1368d95ff1
JisangYun <journeyofbabo@gmail.com>
2026-01-31 18:42:10 +0900

    Add: Point struct, method

--- src/path_planning/breadth_first_search.rs
+++ src/path_planning/breadth_first_search.rs
@@ -6,35 +6,78 @@ use crate::utils::math;
 //}
 
 #[derive(Debug)]
-pub struct StartPosition {
+struct Point {
     x: f32,
     y: f32,
 }
 
-pub struct GoalPosition {
-    x: f32,
-    y: f32,
+impl Point {
+    fn new(x: f32, y: f32) -> Point {
+        Point{
+            x: x,
+            y: y,
+        }
+    }
 }
 
-pub fn use_math() -> i32 {
+struct BFSPlanning {
+    start_position: Point,
+    goal_position: Point,
+}
+
+impl BFSPlanning {
+
+}
+
+
+fn use_math() -> i32 {
     let a = math::sum(2, 3);
     println!("{a}");
     a
 }
 
 pub fn main() {
-    let start_position = StartPosition { x: 10.0, y: 10.0 };
-    let goal_position = GoalPosition { x: 50.0, y: 50.0 };
+    let scenario_1 = BFSPlanning {
+        start_position: Point::new(10.0, 10.0),
+        goal_position: Point::new(50.0, 50.0),
+    };
 
-    //println!("start = {:?}", start_position);
+    // println!("start = {:?}", scenario_0.start_position);
 
-    let mut obstacle_x = Vec::new();
-    let mut obstacle_y = Vec::new();
+    let mut obstacle_x = Vec::new();    // Obstacles x position
+    let mut obstacle_y = Vec::new();    // Obstacles y position
+    let grid_size = 2.0;                // [m]
+    let robot_radius = 1.0;             // [m]
 
+    // Locate Obstacles
     for i in -10..=60 {
         obstacle_x.push(i as f32);
         obstacle_y.push(-10.0);
     }
+    for i in -10..=60 {
+        obstacle_x.push(60.0);
+        obstacle_y.push(i as f32);
+    }
+    for i in -10..=61 {
+        obstacle_x.push(i as f32);
+        obstacle_y.push(60.0);
+    }
+    for i in -10..=61 {
+        obstacle_x.push(-10.0);
+        obstacle_y.push(i as f32);
+    }
+    for i in -10..=40 {
+        obstacle_x.push(20.0);
+        obstacle_y.push(i as f32);
+    }
+    for i in 0..=40 {
+        obstacle_x.push(40.0);
+        obstacle_y.push(60.0 - i as f32);
+    }
 
-    println!("obstacle_x = {:?}", obstacle_x);
+
+
+
+    // println!("use math test {}", use_math());
+    // println!("obstacle_x = {:?}", obstacle_x);
 }
